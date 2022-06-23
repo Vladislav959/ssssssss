ac81adc3c873e864655d84951dc9cba5c03bd425
Vladislav Gubarev <gubarev20084848@gmail.com>
2022-06-23 12:40:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,91 +1,137 @@
 try{
-onepagescroll('.pages',{
-    pageContainer: 'section',     
-    animationType: 'ease-in-out', 
-    animationTime: 700,        
-    infinite: false,           
-    pagination: false,             
-    keyboard: false,               
-    direction: 'vertical'        
+  onepagescroll('.pages',{
+      pageContainer: 'section',     
+      animationType: 'ease-in-out', 
+      animationTime: 700,        
+      infinite: false,           
+      pagination: false,             
+      keyboard: false,               
+      direction: 'vertical'        
+       
+    });
+    let wasSmall = false; 
+    let wasBig = false;
+    
+    const resizeObserver = new ResizeObserver(entries => {
+      if(entries[0].target.clientHeight < 800 && !wasSmall && entries[0].target.clientWidth < 1000){
+          wasSmall = true;
+          wasBig = false
+          console.log( entries[0].target.clientHeight)
+      window.removeEventListener('wheel',onScrollEventHandler);
+          document.body.classList.add("scroll")
+          document.querySelector(".pages").setAttribute("style","transform: translate3d(0px, 0px, 0px);")
+      }
      
+  }
+    )
+    
+    resizeObserver.observe(document.body)
+    const modalcontainer = document.querySelector(".modalcontainer")
+    document.getElementById("openmodal").addEventListener("click", ()=>{
+      modalcontainer.classList.toggle("visible")
+    })
+    document.getElementById("openmodal").addEventListener("focus", ()=>{
+      modalcontainer.classList.toggle("visible")
+    })
+  modalcontainer.querySelector(".modalwall").addEventListener("click", () => {
+    modalcontainer.classList.toggle("visible");
+  })
+  
+  modalcontainer.querySelector(".modalwall").addEventListener("focus", () => {
+    modalcontainer.classList.toggle("visible");
+  })
+    modalcontainer.querySelector(".cross").addEventListener("click",()=>{
+  
+      modalcontainer.classList.toggle("visible")
+    })
+    modalcontainer.querySelector(".cross").addEventListener("focus",()=>{
+  
+      modalcontainer.classList.toggle("visible")
+    })
+  const menu = document.querySelector('.mobilemenu > div');
+  menu.addEventListener('click', () => {
+    
+    
+  document.querySelector(".pages").classList.toggle('menuactive');
+  document.body.classList.toggle('menuactive');
+  document.querySelector(".pages").setAttribute("style","transition: transform 700ms ease-in-out 0s;transform:none!important;")
   });
-  let wasSmall = false; 
-  let wasBig = false;
+  menu.addEventListener('focus', () => {
+    
+    
+    document.querySelector(".pages").classList.toggle('menuactive');
+    document.body.classList.toggle('menuactive');
+    document.querySelector(".pages").setAttribute("style","transition: transform 700ms ease-in-out 0s;transform:none!important;")
+    });
+  let activetab = 0;
+  const tabsButtons = document.querySelectorAll(".techtabs p");
+  tabsButtons.forEach(elem => {
+    elem.addEventListener("click",()=>{
+      const nowIndex = elem.dataset.tabindex
+      if(nowIndex !== activetab){
+  document.querySelector(".activetab").classList.remove("activetab");
+  document.querySelector(`div[data-tabindex="${nowIndex}"]`).classList.add("activetab")
   
-  const resizeObserver = new ResizeObserver(entries => {
-    if(entries[0].target.clientHeight < 800 && !wasSmall && entries[0].target.clientWidth < 1000){
-        wasSmall = true;
-        wasBig = false
-        console.log( entries[0].target.clientHeight)
-		window.removeEventListener('wheel',onScrollEventHandler);
-        document.body.classList.add("scroll")
-        document.querySelector(".pages").setAttribute("style","transform: translate3d(0px, 0px, 0px);")
-    }
-   
-}
-  )
+  document.querySelector(`.techtabs .active`).classList.remove("active")
+  document.querySelector(`p[data-tabindex="${nowIndex}"]`).classList.add("active")
+      activetab = elem.dataset.tabindex;
+      }
+    })
+    elem.addEventListener("focus",()=>{
+      const nowIndex = elem.dataset.tabindex
+      if(nowIndex !== activetab){
+  document.querySelector(".activetab").classList.remove("activetab");
+  document.querySelector(`div[data-tabindex="${nowIndex}"]`).classList.add("activetab")
   
-  resizeObserver.observe(document.body)
-  const modalcontainer = document.querySelector(".modalcontainer")
-  document.getElementById("openmodal").addEventListener("click", ()=>{
-    modalcontainer.classList.toggle("visible")
+  document.querySelector(`.techtabs .active`).classList.remove("active")
+  document.querySelector(`p[data-tabindex="${nowIndex}"]`).classList.add("active")
+      activetab = elem.dataset.tabindex;
+      }
+    })
   })
   
-modalcontainer.querySelector(".modalwall").addEventListener("click", () => {
-  modalcontainer.classList.toggle("visible");
-})
-
-  modalcontainer.querySelector(".cross").addEventListener("click",()=>{
-
-    modalcontainer.classList.toggle("visible")
-  })
-const menu = document.querySelector('.mobilemenu > div');
-menu.addEventListener('click', () => {
-  
-  
-document.querySelector(".pages").classList.toggle('menuactive');
-document.body.classList.toggle('menuactive');
-document.querySelector(".pages").setAttribute("style","transition: transform 700ms ease-in-out 0s;transform:none!important;")
-});
-
-let activetab = 0;
-const tabsButtons = document.querySelectorAll(".techtabs p");
-tabsButtons.forEach(elem => {
-  elem.addEventListener("click",()=>{
-    const nowIndex = elem.dataset.tabindex
-    if(nowIndex !== activetab){
-document.querySelector(".activetab").classList.remove("activetab");
-document.querySelector(`div[data-tabindex="${nowIndex}"]`).classList.add("activetab")
-
-document.querySelector(`.techtabs .active`).classList.remove("active")
-document.querySelector(`p[data-tabindex="${nowIndex}"]`).classList.add("active")
-    activetab = elem.dataset.tabindex;
+  const modalbuttons = document.querySelectorAll(".modalbuttons button");
+  const modalpages = document.querySelector(".modalpages > div")
+  modalbuttons[0].addEventListener("click",()=>{
+    if(!modalbuttons[0].classList.contains("active")){
+      
+      modalpages.style.transform = "translateX(0)"
+      
+      modalbuttons[0].classList.add("active")
+      
+      modalbuttons[1].classList.remove("active")
     }
   })
-})
-
-const modalbuttons = document.querySelectorAll(".modalbuttons button");
-const modalpages = document.querySelector(".modalpages > div")
-modalbuttons[0].addEventListener("click",()=>{
-  if(!modalbuttons[0].classList.contains("active")){
-    
-    modalpages.style.transform = "translateX(0)"
-    
-    modalbuttons[0].classList.add("active")
-    
-    modalbuttons[1].classList.remove("active")
-  }
-})
-modalbuttons[1].addEventListener("click",()=>{
-  if(!modalbuttons[1].classList.contains("active")){
-    
-    modalpages.style.transform = "translateX(calc(-50% + 30px))"
-    
-    modalbuttons[1].classList.add("active")
-    
-    modalbuttons[0].classList.remove("active")
-  }
-})
-				 }
-catch(err){
-	alert(err);}
+  modalbuttons[1].addEventListener("click",()=>{
+    if(!modalbuttons[1].classList.contains("active")){
+      
+      modalpages.style.transform = "translateX(calc(-50% + 30px))"
+      
+      modalbuttons[1].classList.add("active")
+      
+      modalbuttons[0].classList.remove("active")
+    }
+  })
+  modalbuttons[0].addEventListener("focus",()=>{
+    if(!modalbuttons[0].classList.contains("active")){
+      
+      modalpages.style.transform = "translateX(0)"
+      
+      modalbuttons[0].classList.add("active")
+      
+      modalbuttons[1].classList.remove("active")
+    }
+  })
+  modalbuttons[1].addEventListener("focus",()=>{
+    if(!modalbuttons[1].classList.contains("active")){
+      
+      modalpages.style.transform = "translateX(calc(-50% + 30px))"
+      
+      modalbuttons[1].classList.add("active")
+      
+      modalbuttons[0].classList.remove("active")
+    }
+  })
+           }
+  catch(err){
+    alert(err);}
